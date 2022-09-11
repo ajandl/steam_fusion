cf18f16ecd0a6f7959fd31d5eeb4cc58188268d8
Adam Jandl <jandl.wisc@gmail.com>
2022-09-10 21:07:06 -0400

    Implemented .env
    Needs testing.  If successful, envirion_vars can be removed.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from environ_vars import environ_setup
+# from environ_vars import environ_setup
 
 
-environ_setup()  # There may be a easier way using the python-dotenv lib.
+# environ_setup()  # This shouldn't be needed now that .env is implemented.
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
--- app/users_model.py
+++ app/users_model.py
@@ -1,6 +1,7 @@
-from app import db
 from datetime import datetime
+
 from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 
 
 """
